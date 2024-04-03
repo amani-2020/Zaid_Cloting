64c39886c93a1e92698b53e8a33444e1e322ef29
Amani al <amaniheritage@gmail.com>
2024-04-02 18:39:40 -0600

    Add Home  router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import lightimg from './images/lighting.jpg';
-import FaucetImg from './images/faucets.jpg';
-import HeadImg from './images/headlights.jpg';
-import TripodImg from './images/tripods.jpg';
-import Directory from './components/directory/directory.component';
+import Home from "./routes/home/home.component";
 
 
 const App = () =>{
-  const categories= [
-    {
-      "id": 1,
-      "title": "lighting",
-      "imageUrl":lightimg
-    },
-    {
-      "id": 2,
-      "title": "faucets",
-      "imageUrl": FaucetImg
-    },
-    {
-      "id": 3,
-      "title": "headlights",
-      "imageUrl": HeadImg
-    },
-    {
-      "id": 4,
-      "title": "tripods",
-      "imageUrl": TripodImg
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  ;
+  
   return (
-    <Directory categories={categories}/>
+    <Home />
   );
 }
 
